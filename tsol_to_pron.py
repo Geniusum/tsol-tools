c2af22307679ba6b277264a14e0ae6a231523ff2
Genius_um <geniusum.off@gmail.com>
2025-04-24 18:53:35 +0200

    Commit

---
+++ tsol_to_pron.py
@@ -0,0 +1,38 @@
+print("Importing libraries...")
+
+import readline
+from gtts import gTTS
+from playsound import playsound
+import tempfile
+import os
+import tsol.pronunciation as pron
+
+print("Libraries imported.")
+
+translator = pron.TsolToPronunciation()
+vocal_enabled = True
+
+print("'exit' to exit the translation.")
+print("'vocal' to toggle text-to-speech (currently enabled).")
+
+def speak(text: str):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as fp:
+        tts = gTTS(text=text, lang='it')
+        tts.save(fp.name)
+        playsound(fp.name)
+        os.remove(fp.name)
+
+while True:
+    text = input("[Tsol to Pronunciation] : ").strip()
+    if text.lower() == "exit":
+        break
+    elif text.lower() == "vocal":
+        vocal_enabled = not vocal_enabled
+        state = "enabled" if vocal_enabled else "disabled"
+        print(f"Text-to-speech is now {state}.")
+        continue
+    pronunciation = translator.translate(text, vocal_mode=False)
+    if vocal_enabled: vocal_pronunciation = translator.translate(text, vocal_mode=True)
+    print("Pronunciation :", pronunciation)
+    if vocal_enabled:
+        speak(vocal_pronunciation)
